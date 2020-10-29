0a92af227f6b2b3af9370767bf801367507d2a2e
Marc Julian Schwarz <67844154+marcjulianschwarz@users.noreply.github.com>
2020-10-29 12:13:24 +0100

    Update Telekom Datennutzung .js

--- Telekom Datennutzung .js
+++ Telekom Datennutzung .js
@@ -1,6 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: purple; icon-glyph: magic;
+
 const apiURL = "https://pass.telekom.de/api/service/generic/v1/status"
 
 let fm = FileManager.iCloud()
@@ -16,11 +17,10 @@ let telekom_color = new Color("#ea0a8e")
 
 let wifi = false
 
-
 // HELPER CLASS 
 class Telekom{
   constructor(name, usedVolume, initialVolume, remainingVolume, usedPercentage, validUntil, usedAt){
-    this.name = name
+   	this.name = name
     this.usedVolume = usedVolume
     this.inititalVolume = initialVolume
     this.remainingVolume = remainingVolume
@@ -40,22 +40,15 @@ async function isUsingDarkAppearance(){
 
 let darkmode = await isUsingDarkAppearance()
 
-
 async function getFromApi(){
-
     data = ''
     let request = new Request(apiURL)
     request.headers = {
       "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1"
     }
-    
   data = await request.loadJSON()
-  
   data.usedAt = Date.now()
-
   fm.writeString(path, JSON.stringify(data))
-    
-    
     return data
 }
 
@@ -64,11 +57,7 @@ function getFromFile(){
   return data  
 }
 
-
-
 async function getData(){
-
-
   try{
     data_api = await getFromApi()
   }catch{
@@ -76,75 +65,53 @@ async function getData(){
     data_api = await getFromFile()
   }
   
-  
   name = data_api.passName
   usedVolumeNum = data_api.usedVolume / 1024 / 1024 / 1024
   initialVolumeNum = data_api.initialVolume / 1024 / 1024 / 1024
   usedVolume = String(Math.round((usedVolumeNum + Number.EPSILON) * 100) /100).replace('.', ',') + ' GB'
   initialVolume = String(Math.round((initialVolumeNum + Number.EPSILON) * 100) / 100).replace('.', ',') + ' GB'
-  
-  
-  
   remainingVolumeNum = (data_api.initialVolume - data_api.usedVolume) / 1024 / 1024 / 1024
   remainingVolume = String(Math.round((remainingVolumeNum + Number.EPSILON) * 100) / 100) + ' GB'
   remainingVolume = remainingVolume.replace('.', ',')
-  
-  
   usedPercentage = data_api.usedPercentage
-  
   date = data_api.usedAt + data_api.remainingSeconds*1000
   validDate = new Date(date)
   validUntil = df.string(validDate)
   
   telekom = new Telekom(name, usedVolume, initialVolume, remainingVolume, usedPercentage, validUntil)
   
-   
   return telekom
 
 }
 
-
-
 async function createWidget(data){
 
-  
   widget = new ListWidget()
-  
   widget.url = "https://pass.telekom.de"
-  
   
   header_stack = widget.addStack()
   title = header_stack.addText(data.name)
   title.textColor = telekom_color
   
-  
   if (wifi){
-      
     header_stack.addSpacer()
     
     let symbol = SFSymbol.named('wifi.exclamationmark').image
     symbol_image = header_stack.addImage(symbol)
     symbol_image.imageSize = new Size(20, 20)
+    
     if (darkmode){
-      console.log("dark")
       symbol_image.tintColor = new Color("#ffffff")
     }else{
-      console.log("light")
       symbol_image.tintColor = new Color("#000000")
-   }
+    }
   }
   
-  
   widget.addSpacer()
-  
   available_txt = widget.addText(data.remainingVolume + ' von ' + data.inititalVolume + ' noch verfügbar.')
-  
   widget.addSpacer()
-  
   used_txt = widget.addText(data.usedVolume + ' (' + data.usedPercentage + '%) verbraucht.')
-  
   widget.addSpacer()
-  
   footer = widget.addText('Gültig bis: ' + data.validUntil)
   
   title.font = bold_font
@@ -163,32 +130,20 @@ async function createWidget(data){
   }
   
   widget.addSpacer()
-  
-
-  
   return widget
-  
-
 }
-
 
 widget = new ListWidget()
 info = widget.addText('Schalte zur ersten Einrichtung das WLAN aus und starte das Script erneut.')
 info.font = Font.systemFont(13)
 
-
 try {
   data = await getData()
   widget = await createWidget(data)
 }catch {
-  console.log('First init not working or error with fetching data')
+  console.log('First init not working or error while fetching data')
 }
-
-
 
 Script.setWidget(widget)
 widget.presentSmall()  
 Script.complete()
-
-
-// By marc julian schwarz
