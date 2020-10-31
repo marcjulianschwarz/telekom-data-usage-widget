4258be4d1ecbdad82f18f9e58e2d1f2e257b1644
Marc Julian Schwarz <67844154+marcjulianschwarz@users.noreply.github.com>
2020-10-31 16:06:57 +0100

    Update telekom-data-usage.js
    Text im kleinen Widget größer gemacht.
    Jetzt werden auch die restlichen Tage und Stunden bis zur Auffüllung des Datenvolumens angezeigt.

--- telekom-data-usage.js
+++ telekom-data-usage.js
@@ -2,6 +2,7 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: purple; icon-glyph: magic;
 
+
 const apiURL = "https://pass.telekom.de/api/service/generic/v1/status";
 let fm = FileManager.iCloud();
 let p = await args.widgetParameter
@@ -17,17 +18,21 @@ let path = fm.joinPath(dir, "widget-telekom.json");
 let df = new DateFormatter();
 df.useShortDateStyle();
 
-let thin_font = Font.regularRoundedSystemFont(10);
-let bold_font = Font.heavyRoundedSystemFont(10);
+let wifi = false;
+
+// FONTS USED BY THE WIDGET
+let thin_font = Font.regularRoundedSystemFont(13);
+let small_font = Font.regularRoundedSystemFont(11);
+let bold_font = Font.heavyRoundedSystemFont(13);
 
 if(widgetSize == "medium"){
-  thin_font = Font.regularRoundedSystemFont(17);
-  bold_font = Font.heavyRoundedSystemFont(17);
+  thin_font = Font.regularRoundedSystemFont(19);
+  small_font = Font.regularRoundedSystemFont(17);
+  bold_font = Font.heavyRoundedSystemFont(19);
 }
 
+// SPECIAL COLORS
 let telekom_color = new Color("#ea0a8e");
-
-let wifi = false;
 
 // HELPER CLASS 
 class Telekom{
@@ -42,7 +47,7 @@ class Telekom{
   }
 }
 
-// HELPER DARKMODE FUNCTION
+// HELPER DARKMODE FUNCTION (DARKMODE IN WIDGETS STILL NOT WORKING PERFECTLY)
 async function isUsingDarkAppearance(){
   const wv = new WebView();
   let js = "(window.matchMedia && window.matchMedia('(prefers-color-scheme:dark)').matches)";
@@ -52,31 +57,42 @@ async function isUsingDarkAppearance(){
 
 let darkmode = await isUsingDarkAppearance();
 
+// HELPER FUNCTION TO CALCULATE DAYS AND HOURS FOR VALIDUNTIL
+function getDaysHours(data){
+  now = new Date(Date.now())
+  hours_total = (data.validUntil - now) / 1000 / 60 / 60
+  hours = hours_total%24
+  days = (hours_total - hours) / 24
+  return days + "d " + Math.round(hours) + "h"
+}
+
+
+// FETCHING DATE FROM TELEKOM API
 async function getFromApi(){
     data = '';
     let request = new Request(apiURL);
+
     request.headers = {
       "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1"
     };
   data = await request.loadJSON();
-  data.usedAt = Date.now();
+  
+  data.usedAt = Date.now(); // Storing date to calculate validUntil from the remaining seconds
   fm.writeString(path, JSON.stringify(data));
-    return data;
-}
-
-function getFromFile(){
-  data = JSON.parse(fm.readString(path));
   return data;
 }
+
+
 
 async function getData(){
   try{
     data_api = await getFromApi();
   }catch{
     wifi = true;
-    data_api = await getFromFile();
+    data_api = await JSON.parse(fm.readString(path));
   }
   
+  // PREPROCESSING DATA
   var name = data_api.passName;
   var usedVolumeNum = data_api.usedVolume / 1024 / 1024 / 1024;
   var initialVolumeNum = data_api.initialVolume / 1024 / 1024 / 1024;
@@ -90,9 +106,8 @@ async function getData(){
   
   var usedPercentage = data_api.usedPercentage;
   var date = data_api.usedAt + data_api.remainingSeconds*1000;
-  var validDate = new Date(date);
-  var validUntil = df.string(validDate);
-  
+  var validUntil = new Date(date);
+
   var telekom = new Telekom(name, usedVolume, initialVolume, remainingVolume, usedPercentage, validUntil);
   
   return telekom;
@@ -103,17 +118,17 @@ async function createWidget(data){
 
   var widget = new ListWidget();
   widget.url = "https://pass.telekom.de";
-  
+    
   var header_stack = widget.addStack();
   var title = header_stack.addText(data.name);
   title.textColor = telekom_color;
   
   if (wifi){
     header_stack.addSpacer();
-    
+
     let symbol = SFSymbol.named('wifi.exclamationmark').image;
     var symbol_image = header_stack.addImage(symbol);
-    symbol_image.imageSize = new Size(20, 20);
+    symbol_image.imageSize = new Size(17, 17);
     
     if (darkmode){
       symbol_image.tintColor = new Color("#ffffff");
@@ -127,14 +142,17 @@ async function createWidget(data){
   widget.addSpacer();
   var used_txt = widget.addText(data.usedVolume + ' (' + data.usedPercentage + '%) verbraucht.');
   widget.addSpacer();
-  var footer = widget.addText('Gültig bis: ' + data.validUntil);
-  widget.addSpacer();
   
+  var footer = widget.addText('Bis: ' + df.string(data.validUntil).toLocaleString() + ' (' + getDaysHours(data) + ')');
+  
+  
+  // ASSIGNING FONTS
   title.font = bold_font;
   available_txt.font = thin_font;
-  used_txt.font = thin_font;
-  footer.font = thin_font;
+  used_txt.font = bold_font;
+  footer.font = small_font;
   
+  // COLORING BASED ON DATA PERCENTAGE
   if(data.usedPercentage >= 75){
     used_txt.textColor = Color.red();
   }else if(data.usedPercentage >= 50 && data.usedPercentage < 75){
@@ -148,6 +166,7 @@ async function createWidget(data){
   return widget;
 }
 
+// TEMPORARY WIDGET FOR FIRST TIME USERS
 var widget = new ListWidget();
 var info = widget.addText('Schalte zur ersten Einrichtung das WLAN aus und starte das Script erneut.');
 info.font = Font.systemFont(13);
@@ -160,5 +179,5 @@ try {
 }
 
 Script.setWidget(widget);
-widget.presentSmall();
+widget.presentMedium();
 Script.complete();
