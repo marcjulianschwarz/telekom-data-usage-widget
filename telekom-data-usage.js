0ed25c7bcc29de49d60e0660b42dd1593e055458
marcjulianschwarz <info@marc-julian.de>
2022-05-28 00:25:14 +0200

    [MOD] run prettify and fix url not opening

--- telekom-data-usage.js
+++ telekom-data-usage.js
@@ -2,38 +2,40 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: purple; icon-glyph: magic;
 
-
 //########## SETUP ###########
 
-const BACKGROUND_COLOR = Color.dynamic(new Color("#ffffff"), new Color("#161618"));
+const BACKGROUND_COLOR = Color.dynamic(
+  new Color("#ffffff"),
+  new Color("#161618")
+);
 const TELEKOM_COLOR = new Color("#ea0a8e");
 const TEXT_COLOR = Color.dynamic(new Color("000000"), new Color("#ffffff"));
 
 //########## END OF SETUP ##########
 
 const apiURL = "https://pass.telekom.de/api/service/generic/v1/status";
-const imgURL = "https://raw.githubusercontent.com/marcjulianschwarz/telekom-data-usage-widget/main/symbols/"
+const imgURL =
+  "https://raw.githubusercontent.com/marcjulianschwarz/telekom-data-usage-widget/main/symbols/";
 
 let parameters = await args.widgetParameter;
-if(parameters != null){
+if (parameters != null) {
   parameters = parameters.toLowerCase();
-}else{
-  parameters = "icloud"
+} else {
+  parameters = "icloud";
 }
 
 let widgetSize = config.widgetFamily;
 
 let fm = FileManager.iCloud();
-if(parameters.includes("local")){
+if (parameters.includes("local")) {
   fm = FileManager.local();
-}else if(parameters.includes("icloud")){
+} else if (parameters.includes("icloud")) {
   fm = FileManager.iCloud();
 }
 
-
 let dir = fm.joinPath(fm.documentsDirectory(), "telekom-widget");
 let path = fm.joinPath(dir, "telekom-data.json");
-if(!fm.fileExists(dir)){
+if (!fm.fileExists(dir)) {
   fm.createDirectory(dir);
 }
 
@@ -47,18 +49,25 @@ let small_font = Font.regularRoundedSystemFont(11);
 let bold_font = Font.heavyRoundedSystemFont(13);
 let title_font = Font.heavyRoundedSystemFont(11);
 
-if(widgetSize == "medium"){
+if (widgetSize == "medium") {
   thin_font = Font.regularRoundedSystemFont(15);
   small_font = Font.regularRoundedSystemFont(13);
   bold_font = Font.heavyRoundedSystemFont(15);
   title_font = Font.heavyRoundedSystemFont(17);
 }
 
-
-// HELPER CLASS 
-class Telekom{
-  constructor(name, usedVolume, initialVolume, remainingVolume, usedPercentage, validUntil, usedAt){
-   	this.name = name;
+// HELPER CLASS
+class Telekom {
+  constructor(
+    name,
+    usedVolume,
+    initialVolume,
+    remainingVolume,
+    usedPercentage,
+    validUntil,
+    usedAt
+  ) {
+    this.name = name;
     this.usedVolume = usedVolume;
     this.initialVolume = initialVolume;
     this.remainingVolume = remainingVolume;
@@ -69,31 +78,32 @@ class Telekom{
 }
 
 // HELPER FUNCTION TO CALCULATE DAYS AND HOURS FOR VALIDUNTIL
-function getDaysHours(data){
-  now = new Date(Date.now())
-  hours_total = (data.validUntil - now) / 1000 / 60 / 60
-  hours = hours_total%24
-  days = (hours_total - hours) / 24
-  return days + "d " + Math.round(hours) + "h"
+function getDaysHours(data) {
+  now = new Date(Date.now());
+  hours_total = (data.validUntil - now) / 1000 / 60 / 60;
+  hours = hours_total % 24;
+  days = (hours_total - hours) / 24;
+  return days + "d " + Math.round(hours) + "h";
 }
 
 // Get all data and process
-async function getFromApi(){
-    let request = new Request(apiURL);
-    request.headers = {
-      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1"
-    };
-    data = await request.loadJSON();
-    return data;
+async function getFromApi() {
+  let request = new Request(apiURL);
+  request.headers = {
+    "User-Agent":
+      "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1",
+  };
+  data = await request.loadJSON();
+  return data;
 }
 
-async function saveImages(){
-  console.log("loading and saving images")
-  var imgs = ["full", "half", "low", "empty", "almost"]
-  for(img of imgs){
+async function saveImages() {
+  console.log("loading and saving images");
+  var imgs = ["full", "half", "low", "empty", "almost"];
+  for (img of imgs) {
     let img_path = fm.joinPath(dir, img + ".png");
-    if(!fm.fileExists(img_path)){
-      console.log("Loading image: " + img + ".png")
+    if (!fm.fileExists(img_path)) {
+      console.log("Loading image: " + img + ".png");
       let request = new Request(imgURL + img + ".png");
       console.log(request);
       image = await request.loadImage();
@@ -102,73 +112,92 @@ async function saveImages(){
   }
 }
 
-async function getImageFor(name){
+async function getImageFor(name) {
   let img_path = fm.joinPath(dir, name + ".png");
   await fm.downloadFileFromiCloud(img_path);
   img = await fm.readImage(img_path);
   return img;
 }
 
-async function saveData(data){
+async function saveData(data) {
   data.savedDate = Date.now();
   fm.writeString(path, JSON.stringify(data));
   console.log("Saved new data to file");
 }
 
-async function getFromFile(){
+async function getFromFile() {
   await fm.downloadFileFromiCloud(path);
   data = await JSON.parse(fm.readString(path));
   console.log("Fetching data from file was successful");
   return data;
 }
 
-async function processData(data){
+async function processData(data) {
   var name = data.passName;
   var usedVolumeNum = data.usedVolume / 1024 / 1024 / 1024;
   var initialVolumeNum = data.initialVolume / 1024 / 1024 / 1024;
-  
-  var usedVolume = String(Math.round((usedVolumeNum + Number.EPSILON) * 100) /100).replace('.', ',') + ' GB';
-  var initialVolume = String(Math.round((initialVolumeNum + Number.EPSILON) * 100) / 100).replace('.', ',') + ' GB';
-  
-  var remainingVolumeNum = (data.initialVolume - data.usedVolume) / 1024 / 1024 / 1024;
-  var remainingVolume = String(Math.round((remainingVolumeNum + Number.EPSILON) * 100) / 100) + ' GB';
-  remainingVolume = remainingVolume.replace('.', ',');
-  
+
+  var usedVolume =
+    String(Math.round((usedVolumeNum + Number.EPSILON) * 100) / 100).replace(
+      ".",
+      ","
+    ) + " GB";
+  var initialVolume =
+    String(Math.round((initialVolumeNum + Number.EPSILON) * 100) / 100).replace(
+      ".",
+      ","
+    ) + " GB";
+
+  var remainingVolumeNum =
+    (data.initialVolume - data.usedVolume) / 1024 / 1024 / 1024;
+  var remainingVolume =
+    String(Math.round((remainingVolumeNum + Number.EPSILON) * 100) / 100) +
+    " GB";
+  remainingVolume = remainingVolume.replace(".", ",");
+
   var usedPercentage = data.usedPercentage;
-  var date = data.savedDate + data.remainingSeconds*1000 - 60*1000;
+  var date = data.savedDate + data.remainingSeconds * 1000 - 60 * 1000;
   var validUntil = new Date(date);
 
-  var telekom = new Telekom(name, usedVolume, initialVolume, remainingVolume, usedPercentage, validUntil);
-  
+  var telekom = new Telekom(
+    name,
+    usedVolume,
+    initialVolume,
+    remainingVolume,
+    usedPercentage,
+    validUntil
+  );
+
   return telekom;
 }
 
 // Create all widgets
-function createFirstWidget(){
-  first = new ListWidget()
-  first.addText("Wlan für die erste Einrichtung ausschalten und Widget erneut erstellen.")
-  return first
+function createFirstWidget() {
+  first = new ListWidget();
+  first.addText(
+    "Wlan für die erste Einrichtung ausschalten und Widget erneut erstellen."
+  );
+  return first;
 }
 
-async function createErrorWidget(err){
+async function createErrorWidget(err) {
   err_widget = new ListWidget();
   err_widget.addText(err);
   return err_widget;
 }
 
-async function createSmallVisualWidget(data){
-
+async function createSmallVisualWidget(data) {
   var small_visual_widget = new ListWidget();
   var img = await getImageFor("empty");
 
-  if(data.usedPercentage >= 75 && data.usedPercentage <= 95){
+  if (data.usedPercentage >= 75 && data.usedPercentage <= 95) {
     img = await getImageFor("low");
-  }else if(data.usedPercentage >= 50 && data.usedPercentage < 75){
+  } else if (data.usedPercentage >= 50 && data.usedPercentage < 75) {
     img = await getImageFor("half");
-  } else if(data.usedPercentage >= 25 && data.usedPercentage < 50){
+  } else if (data.usedPercentage >= 25 && data.usedPercentage < 50) {
     img = await getImageFor("almost");
-  }else if(data.usedPercentage < 25){
-    img = await getImageFor("full");  
+  } else if (data.usedPercentage < 25) {
+    img = await getImageFor("full");
   }
 
   var widget_image = small_visual_widget.addImage(img);
@@ -177,31 +206,39 @@ async function createSmallVisualWidget(data){
   small_visual_widget.backgroundColor = BACKGROUND_COLOR;
 
   return small_visual_widget;
-
 }
 
-async function createSmallWidget(data){
-
+async function createSmallWidget(data) {
   var widget = new ListWidget();
   var header_stack = widget.addStack();
   var title = header_stack.addText(data.name);
-  
+
   // WIFI Symbol
-  if (wifi){
+  if (wifi) {
     header_stack.addSpacer();
-    let symbol_image = SFSymbol.named('wifi.exclamationmark').image;
+    let symbol_image = SFSymbol.named("wifi.exclamationmark").image;
     var symbol = header_stack.addImage(symbol_image);
     symbol.imageSize = new Size(17, 17);
     symbol.tintColor = TEXT_COLOR;
   }
-  
+
   widget.addSpacer();
-  var used_txt = widget.addText('Benutzt: \n' + data.usedVolume + ' (' + data.usedPercentage + '%)');
-  widget.addSpacer()
-  var available_txt = widget.addText(data.remainingVolume + ' von ' + data.initialVolume);
+  var used_txt = widget.addText(
+    "Benutzt: \n" + data.usedVolume + " (" + data.usedPercentage + "%)"
+  );
   widget.addSpacer();
-  var footer = widget.addText('Bis: ' + df.string(data.validUntil).toLocaleString() + ' (' + getDaysHours(data) + ')');
-  
+  var available_txt = widget.addText(
+    data.remainingVolume + " von " + data.initialVolume
+  );
+  widget.addSpacer();
+  var footer = widget.addText(
+    "Bis: " +
+      df.string(data.validUntil).toLocaleString() +
+      " (" +
+      getDaysHours(data) +
+      ")"
+  );
+
   // ASSIGNING FONTS and COLORS
   title.font = title_font;
   available_txt.font = bold_font;
@@ -213,23 +250,22 @@ async function createSmallWidget(data){
   footer.textColor = TEXT_COLOR;
 
   // COLORING BASED ON DATA PERCENTAGE
-  if(data.usedPercentage >= 75){
+  if (data.usedPercentage >= 75) {
     available_txt.textColor = Color.red();
-  }else if(data.usedPercentage >= 50 && data.usedPercentage < 75){
+  } else if (data.usedPercentage >= 50 && data.usedPercentage < 75) {
     available_txt.textColor = Color.orange();
-  } else if(data.usedPercentage >= 25 && data.usedPercentage < 50){
+  } else if (data.usedPercentage >= 25 && data.usedPercentage < 50) {
     available_txt.textColor = Color.yellow();
-  }else{
-    available_txt.textColor = Color.green();  
+  } else {
+    available_txt.textColor = Color.green();
   }
-  
+
   widget.backgroundColor = BACKGROUND_COLOR;
- 
+
   return widget;
 }
 
-async function createMediumWidget(data){
-
+async function createMediumWidget(data) {
   medium_widget = new ListWidget();
   var body_stack = medium_widget.addStack();
 
@@ -242,36 +278,45 @@ async function createMediumWidget(data){
   info_stack.layoutVertically();
   visual_stack.layoutVertically();
 
-
   var title = info_stack.addText(data.name);
   info_stack.addSpacer();
-  var used_txt = info_stack.addText('Benutzt: ' + data.usedVolume + ' (' + data.usedPercentage + '%).');
-  info_stack.addSpacer()
-  var available_txt = info_stack.addText(data.remainingVolume + ' von ' + data.initialVolume);
+  var used_txt = info_stack.addText(
+    "Benutzt: " + data.usedVolume + " (" + data.usedPercentage + "%)."
+  );
   info_stack.addSpacer();
-  var footer = info_stack.addText('Bis: ' + df.string(data.validUntil).toLocaleString() + ' (' + getDaysHours(data) + ')');
+  var available_txt = info_stack.addText(
+    data.remainingVolume + " von " + data.initialVolume
+  );
+  info_stack.addSpacer();
+  var footer = info_stack.addText(
+    "Bis: " +
+      df.string(data.validUntil).toLocaleString() +
+      " (" +
+      getDaysHours(data) +
+      ")"
+  );
 
   img = await getImageFor("empty");
 
   // COLORING BASED ON DATA PERCENTAGE
-  if(data.usedPercentage >= 75 && data.usedPercentage <= 95){
+  if (data.usedPercentage >= 75 && data.usedPercentage <= 95) {
     available_txt.textColor = Color.red();
     img = await getImageFor("low");
-  }else if(data.usedPercentage >= 50 && data.usedPercentage < 75){
+  } else if (data.usedPercentage >= 50 && data.usedPercentage < 75) {
     available_txt.textColor = Color.orange();
     img = await getImageFor("half");
-  } else if(data.usedPercentage >= 25 && data.usedPercentage < 50){
+  } else if (data.usedPercentage >= 25 && data.usedPercentage < 50) {
     available_txt.textColor = Color.yellow();
     img = await getImageFor("almost");
-  }else if(data.usedPercentage < 25){
+  } else if (data.usedPercentage < 25) {
     available_txt.textColor = Color.green();
-    img = await getImageFor("full");  
-  }else if(data.usedPercentage > 95){
+    img = await getImageFor("full");
+  } else if (data.usedPercentage > 95) {
     available_txt.textColor = Color.red();
   }
 
-  visual_stack.addSpacer()
-  visual_stack.addImage(img)
+  visual_stack.addSpacer();
+  visual_stack.addImage(img);
 
   // ASSIGNING FONTS and COLORS
   title.font = title_font;
@@ -288,18 +333,18 @@ async function createMediumWidget(data){
   return medium_widget;
 }
 
-async function createLargeWidget(data){
+async function createLargeWidget(data) {
   var large_widget = new ListWidget();
   var img = await getImageFor("empty");
 
-  if(data.usedPercentage >= 75 && data.usedPercentage <= 95){
+  if (data.usedPercentage >= 75 && data.usedPercentage <= 95) {
     img = await getImageFor("low");
-  }else if(data.usedPercentage >= 50 && data.usedPercentage < 75){
+  } else if (data.usedPercentage >= 50 && data.usedPercentage < 75) {
     img = await getImageFor("half");
-  } else if(data.usedPercentage >= 25 && data.usedPercentage < 50){
+  } else if (data.usedPercentage >= 25 && data.usedPercentage < 50) {
     img = await getImageFor("almost");
-  }else if(data.usedPercentage < 25){
-    img = await getImageFor("full");  
+  } else if (data.usedPercentage < 25) {
+    img = await getImageFor("full");
   }
 
   var widget_image = large_widget.addImage(img);
@@ -310,47 +355,51 @@ async function createLargeWidget(data){
   return large_widget;
 }
 
-
 // Runtime:
-await saveImages()
+await saveImages();
 
-try{
-  data = await getFromApi()
-  saveData(data)
-}catch{
-  wifi = true
-  console.log("Couldnt fetch data from API. Wifi still on? Trying to read from file.")
+try {
+  data = await getFromApi();
+  saveData(data);
+} catch {
+  wifi = true;
+  console.log(
+    "Couldnt fetch data from API. Wifi still on? Trying to read from file."
+  );
 }
 
-if(!fm.fileExists(path)){
-  console.log("File doesnt exist. Looks like your first init.")
-  first = await createFirstWidget()
-  Script.setWidget(first)
-}else{
-  data = await getFromFile()
-  processedData = await processData(data)
+if (!fm.fileExists(path)) {
+  console.log("File doesnt exist. Looks like your first init.");
+  first = await createFirstWidget();
+  Script.setWidget(first);
+} else {
+  data = await getFromFile();
+  processedData = await processData(data);
 
   widget = createErrorWidget("Widget couldnt be created");
 
-  switch(widgetSize){
-    case "small": 
-      if(parameters.includes("visual")){
+  switch (widgetSize) {
+    case "small":
+      if (parameters.includes("visual")) {
         widget = await createSmallVisualWidget(processedData);
-      }else{
+      } else {
         widget = await createSmallWidget(processedData);
       }
-    break;
-    case "medium": widget = await createMediumWidget(processedData);
-    break;
-    case "large": widget = await createLargeWidget(processedData);
-    break;
-    default: widget = await createSmallWidget(processedData);
+      break;
+    case "medium":
+      widget = await createMediumWidget(processedData);
+      break;
+    case "large":
+      widget = await createLargeWidget(processedData);
+      break;
+    default:
+      widget = await createSmallWidget(processedData);
   }
-  
-  widget.presentSmall()
-  
-  Script.setWidget(widget)
+
+  widget.presentSmall();
+  widget.url = "https://pass.telekom.de";
+
+  Script.setWidget(widget);
 }
 
-
-Script.complete()
+Script.complete();
